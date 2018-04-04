fed292201b8f5b0519252b390b70208c7b902f12
Rongde Qiu <rongde.qiu@outlook.com>
2018-04-03 23:26:04 -0400

    Add full code for EventManager learning

--- EventManager/001.php
+++ EventManager/001.php
@@ -8,6 +8,7 @@
 
 namespace KpEventManager;
 
+use Zend\EventManager\Event;
 use Zend\EventManager\EventManager;
 
 include "../init_autoloader.php";
@@ -15,9 +16,44 @@ include "../init_autoloader.php";
 $eventManager = new EventManager();
 
 // 添加事件Event
-$eventManager->attach('click',function(){
-    echo 'Button clicked.\t';
+// $event包含事件trigger时传递的参数和target对象
+// 可以通过var_dump($event) 来查看具体情况
+$eventManager->attach('click', function (Event $event) {
+    //获取event trigger时的target对象
+    $object = $event->getTarget();
+
+    //获取event trigger时传递给target的参数(数组)
+    $params = $event->getParams();
+
+
+    $object->name = $params['name'];
+    $object->age = (int)$params['age'];
+    $object->sayHi();
 }, 1);
 
+
+// 添加事件Event
+$eventManager->attach('click', function () {
+    echo 'Button clicked second times.\t';
+}, 2);
+
 // 触发事件Event
+// $hello是target 对象
+// 其后是传递的参数数组
+$hello = new SayHi();
+$eventManager->trigger('click', $hello, ['name' => 'Mason', 'age' => 6], function () {
+    echo 'After event triggered.';
+});
+
+
+class SayHi
+{
+    public $name;
+    public $age;
+
+    public function sayHi()
+    {
+        echo 'Hi, ' . $this->name . '. You are ' . $this->age . ' now.';
+    }
+}
 
